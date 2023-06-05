ffdc50128bd14af360dd2fcd7dc4def68f14d9a3
Brian-Enos <brianenos9198@gmail.com>
2023-06-05 11:52:23 +0300

    declared playAroundFunction and implemented the logic behind the game

--- Rock-Paper.js
+++ Rock-Paper.js
@@ -6,4 +6,28 @@ function getComputerChoice() {
   
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+  // Convert playerSelection to lowercase for case-insensitive comparison
+  playerSelection = playerSelection.toLowerCase();
+
+  // Determine the winner of the round
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  }
+  else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
+  else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+// Testing the playRound function
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice(); 
+console.log(playRound(playerSelection, computerSelection));
